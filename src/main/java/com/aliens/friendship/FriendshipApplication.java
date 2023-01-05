e34764c64b16b33ed19fc6824b0624545beefb84
mjj111 <skatks1016@naver.com>
2023-01-05 10:53:42 +0900

    chore: dependecy 추가, 루트파일 수정 JWT, Security, Redis, MySql 의존성 주입 FriendshipApplication 파일 안에 DB 관련 자동 config 설정 제외하던 코드설정 삭제

--- src/main/java/com/aliens/friendship/FriendshipApplication.java
+++ src/main/java/com/aliens/friendship/FriendshipApplication.java
@@ -2,9 +2,8 @@ package com.aliens.friendship;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication()
 public class FriendshipApplication {
 
 	public static void main(String[] args) {
