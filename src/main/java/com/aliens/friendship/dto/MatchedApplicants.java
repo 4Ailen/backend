5006b8e51e2123fa745d9267b5a96686c7c3280a
JeongeunChoi <77786996+JeongeunChoi@users.noreply.github.com>
2023-01-18 22:33:52 +0900

    Issue #26 style: MatchedApplicants에 어노테이션 추가

--- src/main/java/com/aliens/friendship/dto/MatchedApplicants.java
+++ src/main/java/com/aliens/friendship/dto/MatchedApplicants.java
@@ -1,18 +1,17 @@
 package com.aliens.friendship.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Builder
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class MatchedApplicants {
     int memberId1;
     int memberId2;
     Integer memberId3;
 
-    @Builder
-    public MatchedApplicants(int memberId1, int memberId2, Integer memberId3) {
-        this.memberId1 = memberId1;
-        this.memberId2 = memberId2;
-        this.memberId3 = memberId3;
-    }
 }
