2513f5b7900b0ff9299363c864a640789fdd0278
JeongeunChoi <77786996+JeongeunChoi@users.noreply.github.com>
2023-01-18 22:21:04 +0900

    Issue #26 feat: 매칭 컨트롤러 생성
    matchingService 의존관계 주입

---
+++ src/main/java/com/aliens/friendship/controller/MatchingController.java
@@ -0,0 +1,16 @@
+package com.aliens.friendship.controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class MatchingController {
+
+    private final MatchingService matchingService;
+
+    @Autowired
+    public MatchingController(MatchingService matchingService) {
+        this.matchingService = matchingService;
+    }
+}
