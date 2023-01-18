6b46b56e6da3b547a1c10c962330715663c26f43
JeongeunChoi <77786996+JeongeunChoi@users.noreply.github.com>
2023-01-18 22:22:19 +0900

    Issue #26 feat: 매칭 서비스 생성
    repository 의존관계 주입

---
+++ src/main/java/com/aliens/friendship/service/MatchingService.java
@@ -0,0 +1,15 @@
+package com.aliens.friendship.service;
+import com.aliens.friendship.repository.*;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+@Service
+@RequiredArgsConstructor
+public class MatchingService {
+
+    private final LanguageRepository languageRepository;
+    private final QuestionRepository questionRepository;
+    private final MemberRepository memberRepository;
+    private final MatchingParticipantRepository matchingParticipantRepository;
+    private final BlockingInfoRepository blockingInfoRepository;
+
+}
