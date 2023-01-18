f68a1b341e4e3ca768ade7476cd34eba9a00fdf8
JeongeunChoi <77786996+JeongeunChoi@users.noreply.github.com>
2023-01-18 22:28:56 +0900

    Issue #26 style: ApplicantInfo 클래스 내의 변수명 변경
    질문에 대한 답 정보를 가지므로 question을 answer로 변경

--- src/main/java/com/aliens/friendship/dto/ApplicantInfo.java
+++ src/main/java/com/aliens/friendship/dto/ApplicantInfo.java
@@ -1,19 +1,17 @@
 package com.aliens.friendship.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Builder
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class ApplicantInfo {
 
     private int memberId;
-    private int question; // 1 또는 2 값
+    private int answer; // 1 또는 2 값
     private int language;
-
-    @Builder
-    public ApplicantInfo(int memberId, int question, int language) {
-        this.memberId = memberId;
-        this.question = question;
-        this.language = language;
-    }
 }
