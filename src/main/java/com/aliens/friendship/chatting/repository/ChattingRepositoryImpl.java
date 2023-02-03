6a6f79e67595b82a7ad7e3d583ce82a48a795a37
JeongeunChoi <77786996+JeongeunChoi@users.noreply.github.com>
2023-02-03 23:26:23 +0900

    refactor: ChattingRepositoryImpl
    findChattingByMatchingParticipantAndChattingRoom 함수 정의
    MatchingParticipant와 ChattingRoom을 이용하여 같은 채팅방에 있는 상대방 id를 찾아 반환하는 QueryDsl 작성
    
    related to: #49

---
+++ src/main/java/com/aliens/friendship/chatting/repository/ChattingRepositoryImpl.java
@@ -0,0 +1,26 @@
+package com.aliens.friendship.chatting.repository;
+
+import com.aliens.friendship.chatting.domain.Chatting;
+import com.aliens.friendship.chatting.domain.ChattingRoom;
+import com.aliens.friendship.chatting.domain.QChatting;
+import com.aliens.friendship.matching.domain.MatchingParticipant;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class ChattingRepositoryImpl implements ChattingCustomRepository {
+
+    private final JPAQueryFactory queryFactory;
+
+    @Override
+    public Integer findChattingByMatchingParticipantAndChattingRoom(MatchingParticipant matchingParticipant, ChattingRoom chattingRoom) {
+        QChatting qChatting = QChatting.chatting;
+        Chatting chatting = queryFactory
+                .selectFrom(qChatting)
+                .where(qChatting.chattingRoom.id.eq(chattingRoom.getId()).and(qChatting.matchingParticipant.id.ne(matchingParticipant.getId())))
+                .fetchOne();
+
+        return chatting.getMatchingParticipant().getId();
+    }
+
+}
