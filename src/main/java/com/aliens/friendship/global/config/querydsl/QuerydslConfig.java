73592e9daa7cbbc5d28d1803767d665ae48d9516
JeongeunChoi <77786996+JeongeunChoi@users.noreply.github.com>
2023-02-04 00:46:04 +0900

    chore: QuerydslConfig 패키지 이동
    querydsl 패키지 생성 후 QuerydslConfig 클래스를 해당 패키지로 이동
    
    related to: #49

--- src/main/java/com/aliens/friendship/global/config/querydsl/QuerydslConfig.java
+++ src/main/java/com/aliens/friendship/global/config/querydsl/QuerydslConfig.java
@@ -1,4 +1,4 @@
-package com.aliens.friendship.global.config;
+package com.aliens.friendship.global.config.querydsl;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.context.annotation.Bean;
