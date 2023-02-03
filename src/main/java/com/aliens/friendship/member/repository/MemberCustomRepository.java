1d05476379605cff47bff4ff06037ac738805a90
JeongeunChoi <77786996+JeongeunChoi@users.noreply.github.com>
2023-02-04 00:49:21 +0900

    refactor: MemberCustomRepository 생성
    QueryDsl 적용할 함수 findByUsernameWithAuthority 선언
    
    related to: #49

---
+++ src/main/java/com/aliens/friendship/member/repository/MemberCustomRepository.java
@@ -0,0 +1,10 @@
+package com.aliens.friendship.member.repository;
+
+import com.aliens.friendship.member.domain.Member;
+
+import java.util.Optional;
+
+public interface MemberCustomRepository {
+
+    Optional<Member> findByUsernameWithAuthority(String email);
+}
