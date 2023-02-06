060253fdfa3b171aa01d5be449b787e7c91704f5
mjj111 <skatks1016@naver.com>
2023-02-06 20:57:28 +0900

    refactor: Test 결과값을 status로 확인하기 위한 API Controller 수정 API 테스트를 위해 dto 반환이 아닌 ResponseEntity의 status를 갖고 하고자 수정 ref: #51

--- src/main/java/com/aliens/friendship/member/controller/APIController.java
+++ src/main/java/com/aliens/friendship/member/controller/APIController.java
@@ -7,8 +7,10 @@ import com.aliens.friendship.jwt.domain.dto.TokenDto;
 import com.aliens.friendship.member.service.MemberService;
 import com.aliens.friendship.jwt.util.JwtTokenUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequiredArgsConstructor
@@ -17,46 +19,31 @@ public class APIController {
     private final MemberService memberService;
     private final JwtTokenUtil jwtTokenUtil;
 
-    @GetMapping("/health")
-    public String health() {
-        return "OK";
-    }
 
     @PostMapping("/join")
-    public String join(@RequestBody JoinDto joinDto) {
-        memberService.join(joinDto);
-        return "회원가입 완료";
+    public ResponseEntity<JoinDto> join(@RequestBody JoinDto joinDto) {
+        return ResponseEntity.ok(memberService.join(joinDto));
     }
 
-    @PostMapping("/join/admin")
-    public String joinAdmin(@RequestBody JoinDto joinDto) {
-        memberService.joinAdmin(joinDto);
-        return "어드민 회원 가입 완료";
-    }
 
     @PostMapping("/login")
     public ResponseEntity<TokenDto> login(@RequestBody LoginDto loginDto) {
         return ResponseEntity.ok(memberService.login(loginDto));
     }
 
+
+    @PostMapping("/logout")
+    public ResponseEntity<?> logout(@RequestHeader("Authorization") String accessToken,
+                       @RequestHeader("RefreshToken") String refreshToken) {
+        String username = jwtTokenUtil.getUsername(memberService.resolveToken(accessToken));
+        memberService.logout(TokenDto.of(accessToken, refreshToken), username);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+
     @GetMapping("/members/{email}")
     public MemberInfo getMemberInfo(@PathVariable String email) {
         return memberService.getMemberInfo(email);
     }
 
-    @PostMapping("/reissue")
-    public ResponseEntity<TokenDto> reissue(@RequestHeader("RefreshToken") String refreshToken) {
-        return ResponseEntity.ok(memberService.reissue(refreshToken));
-    }
-
-    @PostMapping("/logout")
-    public void logout(@RequestHeader("Authorization") String accessToken,
-                       @RequestHeader("RefreshToken") String refreshToken) {
-        String username = jwtTokenUtil.getUsername(resolveToken(accessToken));
-        memberService.logout(TokenDto.of(accessToken, refreshToken), username);
-    }
-
-    private String resolveToken(String accessToken) {
-        return accessToken.substring(7);
-    }
 }
