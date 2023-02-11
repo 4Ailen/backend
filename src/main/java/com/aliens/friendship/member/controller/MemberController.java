1a43eda7aa5eb772e35effa53d87b5cc6b664452
coke98 <jhs9832@gmail.com>
2023-02-11 17:33:14 +0900

    feat: 회원 정보 반환 컨트롤러 추가
    ref: #69

--- src/main/java/com/aliens/friendship/member/controller/MemberController.java
+++ src/main/java/com/aliens/friendship/member/controller/MemberController.java
@@ -2,12 +2,10 @@ package com.aliens.friendship.member.controller;
 
 import com.aliens.friendship.global.common.Response;
 import com.aliens.friendship.member.controller.dto.JoinDto;
+import com.aliens.friendship.member.controller.dto.MemberInfoDto;
 import com.aliens.friendship.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -21,8 +19,16 @@ public class MemberController {
 
     @PostMapping()
     public Response<String> join(JoinDto joinDto) throws Exception {
+        memberService.join(joinDto);
         return Response.SUCCESS("회원가입 성공");
     }
+
+    @GetMapping("/{email}")
+    public Response<MemberInfoDto> getMember(@PathVariable String email) {
+        return Response.SUCCESS(memberService.getMemberInfo(email));
+    }
+
+
 
 
 }
