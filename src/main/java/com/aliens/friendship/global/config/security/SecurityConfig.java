b87f617911e966c37f2837480c8c68f524773e27
coke98 <jhs9832@gmail.com>
2023-02-19 19:40:10 +0900

    feat: post /member permitAll() 추가
    ref: #35, #69

--- src/main/java/com/aliens/friendship/global/config/security/SecurityConfig.java
+++ src/main/java/com/aliens/friendship/global/config/security/SecurityConfig.java
@@ -41,7 +41,7 @@ public class SecurityConfig {
                 .and()
                 .csrf().disable()
                 .authorizeRequests()
-                .antMatchers(HttpMethod.POST, "/", "/join/**", "/login").permitAll()
+                .antMatchers(HttpMethod.POST, "/", "/join/**", "/login","/member").permitAll()
                 .antMatchers("/health","/logout").authenticated()
                 .anyRequest().hasRole("USER")
 
