ec18c6a37e7a59ea7ef36fa023696151dbdd7a72
JeongeunChoi <77786996+JeongeunChoi@users.noreply.github.com>
2023-02-23 14:53:43 +0900

    refactor: EmailAuthentication 엔티티 수정
    - code 삭제
    - id 자료형 String으로 변경(토큰 값이 저장되므로)
    - expirationTime 추가
    - 이메일 인증 인스턴스 생성 함수 추가
    
    related to: #68

--- src/main/java/com/aliens/friendship/emailAuthentication/domain/EmailAuthentication.java
+++ src/main/java/com/aliens/friendship/emailAuthentication/domain/EmailAuthentication.java
@@ -4,6 +4,8 @@ import lombok.*;
 
 import javax.persistence.*;
 import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.UUID;
 
 @Builder
 @AllArgsConstructor
@@ -15,23 +17,23 @@ import java.time.Instant;
 public class EmailAuthentication {
     public static final String TABLE_NAME = "email_authentication";
     public static final String COLUMN_ID_NAME = "authentication_id";
-    public static final String COLUMN_CODE_NAME = "code";
     public static final String COLUMN_EMAIL_NAME = "email";
     public static final String COLUMN_CREATEDTIME_NAME = "created_time";
+    public static final String COLUMN_EXPIRATIONTIME_NAME = "expiration_time";
 
     public static final String COLUMN_STATUS_NAME = "status";
     @Id
-    @Column(name = COLUMN_ID_NAME, nullable = false)
-    private Integer id;
-
-    @Column(name = COLUMN_CODE_NAME, nullable = false, length = 45)
-    private String code;
+    @Column(name = COLUMN_ID_NAME, nullable = false, length = 36)
+    private String id;
 
     @Column(name = COLUMN_EMAIL_NAME, nullable = false, length = 45)
     private String email;
 
     @Column(name = COLUMN_CREATEDTIME_NAME, nullable = false)
     private Instant createdTime;
+
+    @Column(name = COLUMN_EXPIRATIONTIME_NAME, nullable = false)
+    private Instant expirationTime;
 
     @Enumerated(EnumType.STRING)
     @Builder.Default
@@ -42,4 +44,16 @@ public class EmailAuthentication {
         AUTHENTICATED, NOT_AUTHENTICATED;
     }
 
+    public static EmailAuthentication createEmailAuthentication(String email) {
+        EmailAuthentication emailAuthentication = new EmailAuthentication();
+        emailAuthentication.id = UUID.randomUUID().toString();
+        emailAuthentication.email = email;
+        emailAuthentication.createdTime = Instant.now();
+        emailAuthentication.expirationTime = emailAuthentication.createdTime.plus(5, ChronoUnit.MINUTES);
+        return emailAuthentication;
+    }
+
+    public void updateStatus(Status status) {
+        this.status = status;
+    }
 }
