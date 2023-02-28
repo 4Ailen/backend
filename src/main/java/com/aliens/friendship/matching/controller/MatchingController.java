08c49f9e22f18126d573717b05188e37bbb8b1c1
mjj111 <skatks1016@naver.com>
2023-02-28 15:51:14 +0900

    feat : 차단기능 컨트롤러 추가
    토큰을 기반으로 사용자 정보를 불러와, url로 받은 상대방 memberId를 기반으로 차단기능을 수행하게 만듬
    
    #86

--- src/main/java/com/aliens/friendship/matching/controller/MatchingController.java
+++ src/main/java/com/aliens/friendship/matching/controller/MatchingController.java
@@ -2,28 +2,28 @@ package com.aliens.friendship.matching.controller;
 
 import com.aliens.friendship.global.common.Response;
 import com.aliens.friendship.matching.domain.Question;
+import com.aliens.friendship.matching.service.BlockingInfoService;
 import com.aliens.friendship.matching.service.MatchingService;
 import com.aliens.friendship.matching.service.model.MatchedGroup;
 import com.aliens.friendship.matching.service.model.MatchingParticipantInfo;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 @RestController
+@RequiredArgsConstructor
 public class MatchingController {
 
     private final MatchingService matchingService;
+    private final BlockingInfoService blockingInfoService;
 
-    @Autowired
-    public MatchingController(MatchingService matchingService) {
-        this.matchingService = matchingService;
-    }
 
     @GetMapping("/matching/languages")
     public Response<Map<String, Object>> getLanguages() {
@@ -52,6 +52,15 @@ public class MatchingController {
     public List<MatchedGroup> matchingTeams() {
         // TODO: 본인 제외하고 반환
         return matchingService.teamBuilding();
+    }
+
+    @GetMapping("/matching/partner/{memberId}/block")
+    public Response<String> blocking(@PathVariable int memberId)throws Exception{
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        String email = userDetails.getUsername();
+        blockingInfoService.block(email,memberId);
+        return Response.SUCCESS("차단 성공");
     }
 
     // TODO: 새로 매칭 시작 전 member의 is_applied를 none으로 변경 후 matchingParticipants 데이터 모두 삭제
