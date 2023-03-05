acefcfcd32ca83e1e2126505a58dbda7d9250e67
coke98 <jhs9832@gmail.com>
2023-03-05 19:51:29 +0900

    test: Mock 데이터 생성용 클래스 작성
    - 생성자로부터 받은 사이즈 만큼 언어 리스트 생성
    - 생성자로부터 받은 사이즈 만큼 매칭 참여자 정보 생성
    
    ref: #91

---
+++ src/test/java/com/aliens/friendship/matching/MatchingTestMockData.java
@@ -0,0 +1,38 @@
+package com.aliens.friendship.matching;
+
+import com.aliens.friendship.matching.service.model.Participant;
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+@Getter
+public class MatchingTestMockData {
+    private List<String> mockLanguages = new ArrayList<>();
+    private List<Participant> mockParticipants = new ArrayList<>();
+    private final String[] languageTexts = {"Korean", "English", "Japanese", "Chinese", "Spanish", "French", "German", "Russian", "Arabic", "Portuguese"};
+
+    public MatchingTestMockData(int numberOfLanguages, int numberOfParticipants) {
+        createLanguageTexts(numberOfLanguages);
+        createParticipants(numberOfParticipants);
+    }
+
+    private void createLanguageTexts(int numberOfLanguages) {
+        for (int i = 0; i < numberOfLanguages; i++) {
+            mockLanguages.add(languageTexts[i]);
+        }
+    }
+
+    private void createParticipants(int numberOfParticipants) {
+        Random random = new Random();
+        for (int i = 1; i <= numberOfParticipants; i++) {
+            String firstPreferLanguage = mockLanguages.get(random.nextInt(mockLanguages.size()));
+            String secondPreferLanguage;
+            do { // 1차와 중복 되지 않는 2차 선호 언어 생성
+                secondPreferLanguage = mockLanguages.get(random.nextInt(mockLanguages.size()));
+            } while (secondPreferLanguage.equals(firstPreferLanguage));
+            mockParticipants.add(new Participant(i, firstPreferLanguage, secondPreferLanguage));
+        }
+    }
+}
