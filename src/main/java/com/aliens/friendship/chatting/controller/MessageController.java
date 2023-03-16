3635d6304e7ded7fddb3f1ba78d38d31604c8270
mjj111 <skatks1016@naver.com>
2023-03-16 14:00:16 +0900

    refactor : 컨트롤러 이름 수정 컨트롤러 이름이 비슷한 것들이 많아 수정하게되었으며, 해당 컨트롤러의 경우 데이터 웹소켓을 다루고 있기에 MessageController라고 바꾸게됌 안에 메서드의 경우 채팅 과정에서 저장하게 되는 로직을 추가하게 되어 중간관여하게 작업 방식을 가져 이름을 바꾸게 됌

--- src/main/java/com/aliens/friendship/chatting/controller/MessageController.java
+++ src/main/java/com/aliens/friendship/chatting/controller/MessageController.java
@@ -17,8 +17,7 @@ public class MessageController {
 
     @MessageMapping("/{roomId}") //여기로 전송되면 메서드 호출 -> WebSocketConfig prefixes 에서 적용한건 앞에 생략
     @SendTo("/room/{roomId}")   //구독하고 있는 장소로 메시지 전송 (목적지)  -> WebSocketConfig Broker 에서 적용한건 앞에 붙어줘야됨
-    public com.aliens.friendship.chatting.controller.dto.ChatMessage test(@DestinationVariable Integer roomId, com.aliens.friendship.chatting.controller.dto.ChatMessage message) {
-
+    public com.aliens.friendship.chatting.controller.dto.ChatMessage MessagingInterceptToSave(@DestinationVariable Integer roomId, com.aliens.friendship.chatting.controller.dto.ChatMessage message) {
         //채팅 저장
         ChatMessage chat = chatService.createChat(roomId, message.getSender(), message.getMessage());
         return com.aliens.friendship.chatting.controller.dto.ChatMessage.builder()
