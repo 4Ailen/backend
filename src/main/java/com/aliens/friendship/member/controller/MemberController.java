983db980721b9a2e8614aca36d92ca31cfb5d7ef
JeongeunChoi <77786996+JeongeunChoi@users.noreply.github.com>
2023-03-19 00:58:11 +0900

    refactor: 멤버 컨트롤러 로그인, 로그아웃 API 추가
    - 기존에 API Controller에 있던 로그인, 로그아웃 API를 멤버 컨트롤러로 이동
    
    related to: #88

--- src/main/java/com/aliens/friendship/member/controller/MemberController.java
+++ src/main/java/com/aliens/friendship/member/controller/MemberController.java
@@ -1,6 +1,9 @@
 package com.aliens.friendship.member.controller;
 
 import com.aliens.friendship.global.common.Response;
+import com.aliens.friendship.jwt.domain.dto.LoginDto;
+import com.aliens.friendship.jwt.domain.dto.TokenDto;
+import com.aliens.friendship.jwt.util.JwtTokenUtil;
 import com.aliens.friendship.member.controller.dto.JoinDto;
 import com.aliens.friendship.member.controller.dto.MemberInfoDto;
 import com.aliens.friendship.member.controller.dto.PasswordUpdateRequestDto;
@@ -18,11 +21,25 @@ import java.util.Map;
 public class MemberController {
 
     private final MemberService memberService;
+    private final JwtTokenUtil jwtTokenUtil;
 
     @PostMapping()
     public Response<String> join(JoinDto joinDto) throws Exception {
         memberService.join(joinDto);
         return Response.SUCCESS("회원가입 성공");
+    }
+
+    @PostMapping("/authentication")
+    public Response<TokenDto> login(@RequestBody LoginDto loginDto) {
+        return Response.SUCCESS(memberService.login(loginDto));
+    }
+
+    @DeleteMapping("/authentication")
+    public Response<String> logout(@RequestHeader("Authorization") String accessToken,
+                                   @RequestHeader("RefreshToken") String refreshToken) {
+        String email = jwtTokenUtil.getEmail(memberService.resolveToken(accessToken));
+        memberService.logout(TokenDto.of(accessToken, refreshToken), email);
+        return Response.SUCCESS("로그아웃 성공");
     }
 
     @GetMapping()
