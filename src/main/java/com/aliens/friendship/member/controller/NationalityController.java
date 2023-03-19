17bc33a0137eeddfbae1757ab75431524461e047
JeongeunChoi <77786996+JeongeunChoi@users.noreply.github.com>
2023-03-19 19:34:56 +0900

    refactor: 국적 컨트롤러 수정
    - 어노테이션 Controller에서 RestController로 변경
    - NationalityService 변수 private final 설정
    - 매핑 url "/" 누락되어 추가
    
    related to: #88

--- src/main/java/com/aliens/friendship/member/controller/NationalityController.java
+++ src/main/java/com/aliens/friendship/member/controller/NationalityController.java
@@ -4,18 +4,18 @@ package com.aliens.friendship.member.controller;
 import com.aliens.friendship.global.common.Response;
 import com.aliens.friendship.member.service.NationalityService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
-@RequestMapping("api/v1/member")
+@RequestMapping("/api/v1/member")
 public class NationalityController {
 
-    NationalityService nationalityService;
+    private final NationalityService nationalityService;
 
     @GetMapping("/nationalities")
     public Response<Map<String, Object>> getNationalities() {
