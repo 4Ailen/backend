2efeae262d4d4bf052e10c778f2a9b0272789f1f
JeongeunChoi <77786996+JeongeunChoi@users.noreply.github.com>
2023-03-19 19:50:12 +0900

    feat: 이메일 API 시큐리티 필터 설정
    - 토큰이 불필요한 이메일 API 리소스로의 접근을 인증절차 없이 허용
      - 인증 이메일 발송 요청
      - 이메일 본인 인증
    
    related to: #88

--- src/main/java/com/aliens/friendship/global/config/security/SecurityConfig.java
+++ src/main/java/com/aliens/friendship/global/config/security/SecurityConfig.java
@@ -41,8 +41,8 @@ public class SecurityConfig {
                 .and()
                 .csrf().disable()
                 .authorizeRequests()
-                .antMatchers(HttpMethod.POST, "/", "/join/**", "/login", "/api/v1/member/authentication", "/api/v1/member", "/api/v1/member/{email}/password/temp").permitAll()
-                .antMatchers(HttpMethod.GET, "/api/v1/member/email/{email}/existence", "/api/v1/member/{email}/authentication-status", "/api/v1/member/nationalities").permitAll()
+                .antMatchers(HttpMethod.POST, "/", "/join/**", "/login", "/api/v1/member/authentication", "/api/v1/member", "/api/v1/member/{email}/password/temp", "/api/v1/email/**").permitAll()
+                .antMatchers(HttpMethod.GET, "/api/v1/member/email/{email}/existence", "/api/v1/member/{email}/authentication-status", "/api/v1/member/nationalities", "/api/v1/email/**").permitAll()
                 .antMatchers("/health", "/logout").authenticated()
                 .anyRequest().hasRole("USER")
 
