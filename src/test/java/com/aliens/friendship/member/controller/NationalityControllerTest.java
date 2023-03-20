a88affc37c1bb869153254316a0426e49389894d
JeongeunChoi <77786996+JeongeunChoi@users.noreply.github.com>
2023-03-20 15:09:10 +0900

    test: 국적 컨트롤러 테스트 코드 작성
    - 국적 목록 요청 성공 테스트 케이스
    
    related to: #88

---
+++ src/test/java/com/aliens/friendship/member/controller/NationalityControllerTest.java
@@ -0,0 +1,55 @@
+package com.aliens.friendship.member.controller;
+
+import com.aliens.friendship.global.config.jwt.JwtAuthenticationFilter;
+import com.aliens.friendship.member.domain.Nationality;
+import com.aliens.friendship.member.service.NationalityService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.*;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc(addFilters = false)
+@WebMvcTest(NationalityController.class)
+class NationalityControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @MockBean
+    private NationalityService nationalityService;
+
+    @Test
+    @DisplayName("국적 목록 요청 성공")
+    void GetNationalities_Success() throws Exception {
+        // given
+        Nationality nationality1 = new Nationality(1, "USA");
+        Nationality nationality2 = new Nationality(2, "Korea");
+        List<Nationality> nationalities = Arrays.asList(nationality1, nationality2);
+        Map<String, Object> nationalitiesMap = new HashMap<>();
+        nationalitiesMap.put("nationalities", nationalities);
+        when(nationalityService.getNationalities()).thenReturn(nationalitiesMap);
+
+        // when & then
+        mockMvc.perform(get("/api/v1/member/nationalities")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.success").value(true))
+                .andExpect(jsonPath("$.response.nationalities.*", hasSize(nationalities.size())));
+        verify(nationalityService, times(1)).getNationalities();
+    }
+}
