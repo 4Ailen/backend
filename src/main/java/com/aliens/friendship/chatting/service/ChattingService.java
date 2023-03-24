772a6f8b2606d7b621328846444a0867055bfb72
mjj111 <skatks1016@naver.com>
2023-03-24 13:45:01 +0900

    refactor : 채팅목록 접근 시 필요한 정보를 제공하는 서비스 로직 구현, 방 생성 , 채팅 메시지 저장, 방 상태 변경 , 방정보 가져오기 기능 구현
    #90

--- src/main/java/com/aliens/friendship/chatting/service/ChattingService.java
+++ src/main/java/com/aliens/friendship/chatting/service/ChattingService.java
@@ -24,72 +24,49 @@ public class ChattingService {
     private final ChattingRoomRepository chattingRoomRepository;
     private final MatchingParticipantRepository matchingParticipantRepository;
     private final ChattingRepository chattingRepository;
-    private final ChatMessageRepository chatRepository;
+    private final ChatMessageRepository chatMessageRepository;
 
-    /**
-     * 모든 채팅방 찾기
-     */
-    public List<ChattingRoom> findAllRoom() {
-        return chattingRoomRepository.findAll();
-    }
 
-    /**
-     * 특정 채팅방 찾기
-     * @param id room_id
-     */
-    public ChattingRoom findRoomById(Integer id) {
-        return chattingRoomRepository.findById(id).orElseThrow();
-    }
 
-    /**
-     * 채팅방 만들기
-     * @param name 방 이름
-     */
-    @Transactional(readOnly = false)
-    public ChattingRoom createRoom() {
-        return chattingRoomRepository.save(new ChattingRoom());
-    }
 
-    /////////////////
-
-    /**
-     * 채팅 생성
-     * @param roomId 채팅방 id
-     * @param sender 보낸이
-     * @param message 내용
-     */
-    @Transactional(readOnly = false)
-    public ChatMessage createChat(Integer roomId, String sender, String message) {
-        ChattingRoom room = chattingRoomRepository.findById(roomId).orElseThrow(() -> new NoSuchElementException("Can't find room "+roomId));
-        return chatRepository.save(ChatMessage.createChat(room.getId(), sender, message));
-    }
-
-    /**
-     * 채팅방 채팅내용 불러오기
-     * @param roomId 채팅방 id
-     */
-    public List<ChatMessage> findAllChatByRoomId(Long roomId) {
-        return chatRepository.findAllByRoom(roomId);
-    }
-
-    @Transactional(readOnly = false)
-    public void updateRoomStatus(int roomId, String status) {
-        ChattingRoom room = chattingRoomRepository.findById(roomId).orElseThrow(()-> new NoSuchElementException("Can't find room "+roomId));
-        ChattingRoom.RoomStatus roomStatus = ChattingRoom.RoomStatus.valueOf(status.toUpperCase());
-        room.updateStatus(roomStatus);
-        chattingRoomRepository.save(room);
-    }
-
-    public List<RoomInfoDto> getRoomInfoDtoListByMatchingParticipantId(Integer matchingParticipantId) {
-        List<RoomInfoDto> roomInfoDtoList = new ArrayList<>();
-        MatchingParticipant matchingParticipant = matchingParticipantRepository.findById(matchingParticipantId).orElseThrow(() -> new NoSuchElementException("Can't find matchingParticipant " + matchingParticipantId));
+    public List<RoomInfoDto> getRoomInfoDtosByMatchingParticipantId(Integer currentMemberId) {
+        List<RoomInfoDto> roomInfoDtos = new ArrayList<>();
+        MatchingParticipant matchingParticipant = matchingParticipantRepository.findById(currentMemberId).orElseThrow(() -> new NoSuchElementException("Can't find matchingParticipant "));
         for(Chatting chatting : chattingRepository.findByMatchingParticipant(matchingParticipant)){
             RoomInfoDto roomInfoDto = new RoomInfoDto();
             roomInfoDto.setRoomId(chatting.getChattingRoom().getId());
             roomInfoDto.setStatus(chatting.getChattingRoom().getStatus().toString());
             roomInfoDto.setPartnerId(chattingRepository.findPartnerIdByMatchingParticipantAndChattingRoom(matchingParticipant, chatting.getChattingRoom()));
-            roomInfoDtoList.add(roomInfoDto);
+            roomInfoDto.setChatMessages(findAllChatMessageByRoomId(chatting.getChattingRoom().getId()));
+            roomInfoDtos.add(roomInfoDto);
         }
-        return roomInfoDtoList;
+        return roomInfoDtos;
+    }
+
+
+    @Transactional
+    public ChattingRoom createRoom() {
+        return chattingRoomRepository.save(new ChattingRoom());
+    }
+
+
+    @Transactional
+    public ChatMessage saveChatMessage(Long roomId, String sender, String message,Integer category) {
+        // 실제 있는 roomId인지 검증
+        ChattingRoom room = chattingRoomRepository.findById(roomId).orElseThrow(() -> new NoSuchElementException("Can't find room "+roomId));
+        return chatMessageRepository.save(ChatMessage.createChat(room.getId(), sender, message,category));
+    }
+
+
+    @Transactional
+    public void updateRoomStatus(Long roomId, String status) {
+        ChattingRoom room = chattingRoomRepository.findById(roomId).orElseThrow(()-> new NoSuchElementException("Can't find room "+roomId));
+        ChattingRoom.RoomStatus roomStatus = ChattingRoom.RoomStatus.valueOf(status.toUpperCase());
+        room.updateStatus(roomStatus);
+    }
+
+
+    public List<ChatMessage> findAllChatMessageByRoomId(Long roomId) {
+        return chatMessageRepository.findAllByRoom(roomId);
     }
 }
