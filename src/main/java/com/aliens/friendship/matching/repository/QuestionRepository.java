d0b8e0f19967bc81592a14ab31e217da58553704
mjj111 <skatks1016@naver.com>
2023-03-24 13:31:50 +0900

    remove : 사용하지 않는 파일 삭제
    더 이상 사용하지 않는 파일에 해당되어 삭제하게 되었습니다.
    
    #90

--- src/main/java/com/aliens/friendship/matching/repository/QuestionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.aliens.friendship.matching.repository;
-
-import com.aliens.friendship.matching.domain.Question;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QuestionRepository extends JpaRepository<Question, Integer> {
-    Question findByIsSelected(byte isSelected);
-}
