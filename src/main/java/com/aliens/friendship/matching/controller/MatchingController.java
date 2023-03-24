7109311ea3f4799e4cafea9c41000b9cdd6855ea
mjj111 <skatks1016@naver.com>
2023-03-24 13:42:44 +0900

    refactor : blockinginfo 로직에서 방 닫기,  차단 알림메시지 추가 및 roomId 값 URL로 받기
    차단 로직이 변경되었습니다.상대의 id와 방의 id값을 받아, 상대 차단 , 방 close 상태변경, 방 입장시 닫긴 방이라는 알림 메시지 전달을 수행하게 되었습니다.
    
    #90

--- src/main/java/com/aliens/friendship/matching/controller/MatchingController.java
+++ src/main/java/com/aliens/friendship/matching/controller/MatchingController.java
@@ -1,12 +1,11 @@
 package com.aliens.friendship.matching.controller;
 
+import com.aliens.friendship.chatting.service.ChattingService;
 import com.aliens.friendship.global.common.Response;
-import com.aliens.friendship.matching.domain.Question;
 import com.aliens.friendship.matching.service.MatchingInfoService;
 import com.aliens.friendship.matching.controller.dto.MatchingParticipantInfo;
 import com.aliens.friendship.matching.service.BlockingInfoService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -20,11 +19,9 @@ import java.util.Map;
 public class MatchingController {
 
     private final MatchingInfoService matchingInfoService;
+    private final BlockingInfoService blockingInfoService;
+    private final ChattingService chattingService;
 
-    @Autowired
-    public MatchingController(MatchingInfoService matchingInfoService) {
-        this.matchingInfoService = matchingInfoService;
-    }
 
     @GetMapping("/matching/languages")
     public Response<Map<String, Object>> getLanguages() {
@@ -46,12 +43,14 @@ public class MatchingController {
     }
 
 
-    @GetMapping("/matching/partner/{memberId}/block")
-    public Response<String> blocking(@PathVariable int memberId)throws Exception{
+    @GetMapping("/matching/partner/{memberId}/block/{roomId}")
+    public Response<String> blocking(@PathVariable int memberId,Long roomId){
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         UserDetails userDetails = (UserDetails) authentication.getPrincipal();
         String email = userDetails.getUsername();
         blockingInfoService.block(email,memberId);
+        chattingService.saveChatMessage(roomId,"공지","차단된 상대입니다.",3);
+        chattingService.updateRoomStatus(roomId, "CLOSE");
         return Response.SUCCESS("차단 성공");
     }
 
