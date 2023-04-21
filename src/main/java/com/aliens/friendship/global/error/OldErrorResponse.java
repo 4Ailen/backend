b79d02e27fe1db039dbe8ee717e464eaa5c77e06
pepe6bf <pepe6bf@gmail.com>
2023-04-21 17:35:19 +0900

    Remove: #112 기존에 존재하던 ErrorResponse & GlobalExceptionHandler 제거
    기존에 존재하던 ErrorResponse와 GlobalExceptionHandler을 제거합니다.
    
    다만 기존 코드와의 호환성 문제를 염두해서 실제 물리적으로 제거하는게 아닌, Class이름을 Old ~로 변경 후 전체 소스코드를 주석처리하였습니다.

---
+++ src/main/java/com/aliens/friendship/global/error/OldErrorResponse.java
@@ -0,0 +1,26 @@
+//package com.aliens.friendship.global.error;
+//
+//import lombok.Getter;
+//import org.springframework.http.HttpStatus;
+//
+//@Getter
+//public class ErrorResponse {
+//
+//    private final String MESSAGE = "message";
+//    private final String STATUS = "status";
+//    private final String msg;
+//    private final int status;
+//
+//    public ErrorResponse(String msg, int status) {
+//        this.msg = msg;
+//        this.status = status;
+//    }
+//
+//    public ErrorResponse(String msg, HttpStatus status) {
+//        this(msg, status.value());
+//    }
+//
+//    public ErrorResponse(Throwable throwable, HttpStatus status) {
+//        this(throwable.getMessage(), status.value());
+//    }
+//}
