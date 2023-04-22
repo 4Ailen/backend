c795730dad687fe28094b49e05bf1f9b235459be
pepe6bf <pepe6bf@gmail.com>
2023-04-22 20:23:52 +0900

    refactor: #112 GlobalExceptionHandler 리펙토링
    기존 GlobalExceptionHandler를 다음과 같이 리펙토링하였습니다.
    
    - 예외가 발생할 경우 Exception Code를 바탕으로 일관된 예외 메시지를 클라이언트에 응답할 수 있도록 개선
    - 새롭게 정의한 커스텀 예외를 반영
    - Bean Validation 예외 반영

--- src/main/java/com/aliens/friendship/global/error/GlobalExceptionHandler.java
+++ src/main/java/com/aliens/friendship/global/error/GlobalExceptionHandler.java
@@ -1,33 +1,89 @@
 package com.aliens.friendship.global.error;
 
-import com.aliens.friendship.global.common.Response;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpHeaders;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.validation.BindException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import static com.aliens.friendship.global.common.Response.ERROR;
+import static com.aliens.friendship.global.error.GlobalExceptionCode.*;
 
-@ControllerAdvice
+@Slf4j
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    private final Logger log = LoggerFactory.getLogger(getClass());
-
-    private ResponseEntity<Response<?>> newResponse(Throwable throwable, HttpStatus status) {
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Content-Type", "application/json");
-        return new ResponseEntity<>(ERROR(throwable, status), headers, status);
+    /**
+     * 존재하지 않는 자원 접근 예외 핸들링
+     * Custom Exception
+     */
+    @ExceptionHandler(ResourceNotFoundException.class)
+    protected ResponseEntity<ErrorResponse> handleResourceNotFoundException(
+            ResourceNotFoundException e
+    ) {
+        log.error("handle ResourceNotFoundException");
+        return new ResponseEntity<>(
+                ErrorResponse.of(e.getCode()),
+                HttpStatus.valueOf(e.getCode().getHttpStatus().value())
+        );
     }
 
-    @ExceptionHandler({
-            IllegalArgumentException.class,
-    })
-    public ResponseEntity<?> handleBadRequestException(Exception e) {
-        log.debug("Bad request exception occurred: {}", e.getMessage(), e);
-        return newResponse(e, HttpStatus.BAD_REQUEST);
+    /**
+     * 요청 파라미터 검증 예외 핸들링
+     * Built-In Exception
+     */
+    @ExceptionHandler(BindException.class)
+    protected ResponseEntity<ErrorResponse> handleBindException(BindException e) {
+        log.error("handle BindException");
+        return new ResponseEntity<>(
+                ErrorResponse.of(INVALID_REQUEST_PARAMETER, e.getBindingResult()),
+                HttpStatus.valueOf(INVALID_REQUEST_PARAMETER.getHttpStatus().value())
+        );
     }
 
+    /**
+     * 유효하지 않은 HTTP Method 요청 예외 핸들링
+     * Built-In Exception
+     */
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    protected ResponseEntity<ErrorResponse> handleHttpRequestMethodNotSupportedException(
+            HttpRequestMethodNotSupportedException e
+    ) {
+        log.error("handle HttpRequestMethodNotSupportedException");
+        return new ResponseEntity<>(
+                ErrorResponse.of(INVALID_REQUEST_METHOD),
+                HttpStatus.valueOf(INVALID_REQUEST_METHOD.getHttpStatus().value())
+        );
+    }
+
+    /**
+     * 유효하지 않은 리소스 소유자의 요청 예외 핸들링
+     * Custom Exception
+     */
+    @ExceptionHandler(InvalidResourceOwnerException.class)
+    protected ResponseEntity<ErrorResponse> handleInvalidResourceOwnerException(
+            InvalidResourceOwnerException e
+    ) {
+        log.error("handle InvalidResourceOwnerException", e);
+        return new ResponseEntity<>(
+                ErrorResponse.of(e.getCode()),
+                HttpStatus.valueOf(e.getCode().getHttpStatus().value())
+        );
+    }
+
+    /**
+     * 최상위 예외 핸들링
+     * Built-In Exception
+     */
+    @ExceptionHandler(Exception.class)
+    protected ResponseEntity<ErrorResponse> handleException(
+            Exception e
+    ) {
+        log.error("handle Exception", e);
+        return new ResponseEntity<>(
+                ErrorResponse.of(SERVER_ERROR),
+                HttpStatus.valueOf(SERVER_ERROR.getHttpStatus().value())
+        );
+    }
 }
