d047e03ee890614558034680f65adfadc0431dc3
pepe6bf <pepe6bf@gmail.com>
2023-04-23 19:25:03 +0900

    refactor: #112 GlobalExceptionCode 에러코드 형식 변경
    CM -> GB 로 변경

--- src/main/java/com/aliens/friendship/global/error/GlobalExceptionCode.java
+++ src/main/java/com/aliens/friendship/global/error/GlobalExceptionCode.java
@@ -11,10 +11,10 @@ import static org.springframework.http.HttpStatus.*;
 public enum GlobalExceptionCode
         implements ExceptionCode {
 
-    INVALID_REQUEST_PARAMETER(BAD_REQUEST, "CM-C-001", "유효하지 않은 요청 파라미터입니다."),
-    INVALID_REQUEST_METHOD(METHOD_NOT_ALLOWED, "CM-C-002", "유효하지 않은 http 요청 메소드입니다."),
-    INVALID_RESOURCE_OWNER(FORBIDDEN, "CM-C-003", "해당 리소스를 처리할 권한이 없습니다."),
-    SERVER_ERROR(INTERNAL_SERVER_ERROR, "CM-S-001", "Internal Server Error");
+    INVALID_REQUEST_PARAMETER(BAD_REQUEST, "GB-C-001", "유효하지 않은 요청 파라미터입니다."),
+    INVALID_REQUEST_METHOD(METHOD_NOT_ALLOWED, "GB-C-002", "유효하지 않은 http 요청 메소드입니다."),
+    INVALID_RESOURCE_OWNER(FORBIDDEN, "GB-C-003", "해당 리소스를 처리할 권한이 없습니다."),
+    SERVER_ERROR(INTERNAL_SERVER_ERROR, "GB-S-001", "Internal Server Error");
 
     private final HttpStatus httpStatus;
     private final String code;
