85eb0c3b4cce03b7fdc99238199b9b3b132028a2
pepe6bf <pepe6bf@gmail.com>
2023-04-23 19:26:40 +0900

    refactor: #112 Response 에러 응답 로직 제거
    기존 Response에서 담당하던 에러 응답 로직을 현재는 ErrorResponse와 각 예외 Handler가 수행하도록 리펙토링 하였으므로 해당 로직은 불필요하다고 판단되어 제거한다.

--- src/main/java/com/aliens/friendship/global/common/Response.java
+++ src/main/java/com/aliens/friendship/global/common/Response.java
@@ -2,7 +2,6 @@ package com.aliens.friendship.global.common;
 
 import com.aliens.friendship.global.error.ErrorResponse;
 import lombok.Getter;
-import org.springframework.http.HttpStatus;
 
 @Getter
 public class Response<T> {
@@ -19,15 +18,7 @@ public class Response<T> {
         this.error = error;
     }
 
-    public static <T> Response<T> SUCCESS(T response){
+    public static <T> Response<T> SUCCESS(T response) {
         return new Response<>(true, response, null);
-    }
-
-    public static <T> Response<T> ERROR(String msg, HttpStatus status){
-        return new Response<>(false, null, new ErrorResponse(msg, status));
-    }
-
-    public static <T> Response<T> ERROR(Throwable throwable, HttpStatus status){
-        return new Response<>(false, null, new ErrorResponse(throwable, status));
     }
 }
