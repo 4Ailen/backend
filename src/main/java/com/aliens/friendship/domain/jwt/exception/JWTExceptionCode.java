323f44250f6df8a1fe1e9501779d289c250e18ac
pepe6bf <pepe6bf@gmail.com>
2023-05-15 01:21:01 +0900

    exception: #128 JWTExceptionCode에 예외 코드 추가

--- src/main/java/com/aliens/friendship/domain/jwt/exception/JWTExceptionCode.java
+++ src/main/java/com/aliens/friendship/domain/jwt/exception/JWTExceptionCode.java
@@ -13,7 +13,8 @@ public enum JWTExceptionCode
         implements ExceptionCode {
 
     REFRESH_TOKEN_NOT_FOUND(NOT_FOUND, "AT-C-001", "Refresh Token이 존재하지 않습니다."),
-    INVALID_REFRESH_TOKEN(UNAUTHORIZED, "AT-C-002", "유효하지 않은 Refresh Token 입니다.");
+    INVALID_REFRESH_TOKEN(UNAUTHORIZED, "AT-C-002", "유효하지 않은 Refresh Token 입니다."),
+    INVALID_TOKEN(UNAUTHORIZED, "AT-C-002", "유효하지 않은 토큰입니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
