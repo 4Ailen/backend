db554899d6fed9ebf577364b089631af287c7587
JeongeunChoi <77786996+JeongeunChoi@users.noreply.github.com>
2023-05-20 01:07:51 +0900

    exception: 일주일 내에 탈퇴한 회원이 회원가입하는 경우 예외 추가
    - 일주일 내에 탈퇴한 회원의 경우 db에 회원의 정보가 남아있으므로, 회원가입이 불가함을 알리는 예외 상황 정의
    
    related to: BACKEND-58

--- src/main/java/com/aliens/friendship/domain/member/exception/MemberExceptionHandler.java
+++ src/main/java/com/aliens/friendship/domain/member/exception/MemberExceptionHandler.java
@@ -57,6 +57,21 @@ public class MemberExceptionHandler {
     }
 
     /**
+     * WithdrawnWithinAWeekException 핸들링
+     * Custom Exception
+     */
+    @ExceptionHandler(WithdrawnMemberWithinAWeekException.class)
+    protected ResponseEntity<ErrorResponse> handlingWithdrawnWithinAWeekException(
+            WithdrawnMemberWithinAWeekException e
+    ) {
+        log.error("[handling WithdrawnWithinAWeekException] {}", e.getExceptionCode().getMessage());
+        return new ResponseEntity<>(
+                ErrorResponse.of(e.getExceptionCode()),
+                HttpStatus.valueOf(e.getExceptionCode().getHttpStatus().value())
+        );
+    }
+
+    /**
      * EmailVerificationException 핸들링
      * Custom Exception
      */
