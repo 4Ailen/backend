4962a30a2b12e70bda38f1d9fc5dbfd683184d37
JeongeunChoi <77786996+JeongeunChoi@users.noreply.github.com>
2023-05-20 20:44:22 +0900

    exception: 임시 비밀번호 발급 시 이름이 잘못된 경우 예외 추가
    - 임시 비밀번호 발급을 위해 이메일과 이름 입력 시, 잘못된 값에 대해 상세히 알려주기 위해 이름이 잘못된 경우를 알리는 예외 상황 정의
    
    related to: BACKEND-58

--- src/main/java/com/aliens/friendship/domain/member/exception/MemberExceptionCode.java
+++ src/main/java/com/aliens/friendship/domain/member/exception/MemberExceptionCode.java
@@ -18,6 +18,7 @@ public enum MemberExceptionCode
     PASSWORD_CHANGE_FAILED_EXCEPTION(BAD_REQUEST, "MB-C-004", "비밀번호 변경에 실패하였습니다."),
     EMAIL_VERIFICATION_NOT_COMPLETED(FORBIDDEN, "MB-C-005", "이메일 인증이 완료되지 않았습니다."),
     WITHDRAWN_MEMBER_WITHIN_A_WEEK(CONFLICT, "MB-C-006", "일주일 내에 탈퇴한 회원은 동일한 이메일로 가입할 수 없습니다."),
+    INVALID_MEMBER_NAME(BAD_REQUEST, "MB-C-007", "회원의 이름이 일치하지 않습니다."),
     NATIONALITIES_NOT_FOUND(NOT_FOUND, "NTN-C-001", "국적 목록이 존재하지 않습니다.");
 
     private final HttpStatus httpStatus;
--- src/main/java/com/aliens/friendship/domain/member/exception/MemberExceptionHandler.java
+++ src/main/java/com/aliens/friendship/domain/member/exception/MemberExceptionHandler.java
@@ -72,6 +72,21 @@ public class MemberExceptionHandler {
     }
 
     /**
+     * InvalidMemberNameException 핸들링
+     * Custom Exception
+     */
+    @ExceptionHandler(InvalidMemberNameException.class)
+    protected ResponseEntity<ErrorResponse> handlingInvalidMemberNameException(
+            InvalidMemberNameException e
+    ) {
+        log.error("[handling InvalidMemberNameException] {}", e.getExceptionCode().getMessage());
+        return new ResponseEntity<>(
+                ErrorResponse.of(e.getExceptionCode()),
+                HttpStatus.valueOf(e.getExceptionCode().getHttpStatus().value())
+        );
+    }
+
+    /**
      * EmailVerificationException 핸들링
      * Custom Exception
      */
