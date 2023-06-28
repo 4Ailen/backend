cb229a9dab8173d54759c9143ba6ade9515ffe40
JeongeunChoi <77786996+JeongeunChoi@users.noreply.github.com>
2023-06-28 17:59:14 +0900

    refactor: nationality 관련 코드 삭제
    - 프론트에서 보내는 국적 값을 문자열 그대로 db에 저장함에 따라 nationality의 필요성이 없어져 삭제
    
    related to: #145

--- src/main/java/com/aliens/friendship/domain/member/repository/NationalityRepository.java
+++
@@ -1,7 +0,0 @@
-package com.aliens.friendship.domain.member.repository;
-
-import com.aliens.friendship.domain.member.domain.Nationality;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NationalityRepository extends JpaRepository<Nationality, Integer> {
-}
