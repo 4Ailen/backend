fce06696e07143635178ad41d76d8761792a43b5
김명준 <86913355+mjj111@users.noreply.github.com>
2023-06-30 12:44:00 +0900

    refactor : 매칭 서버에서 제공할 수 있는 채팅방의 정보제공을 위한 내용 수정 #136

--- src/main/java/com/aliens/friendship/domain/auth/dto/RoomInfoDto.java
+++ src/main/java/com/aliens/friendship/domain/auth/dto/RoomInfoDto.java
@@ -1,9 +1,6 @@
 package com.aliens.friendship.domain.auth.dto;
 
-import com.aliens.friendship.domain.chatting.domain.ChatMessage;
 import lombok.*;
-
-import java.util.List;
 
 @Getter @Setter
 @NoArgsConstructor
@@ -11,6 +8,6 @@ import java.util.List;
 public class RoomInfoDto {
     private Long roomId;
     private String status;
-    private List<ChatMessage> chatMessages;
     private Integer partnerId;
+
 }
