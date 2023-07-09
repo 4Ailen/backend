5b8f1be9da76ba81f5e028024cf6c6fbefd3ff99
JeongeunChoi <77786996+JeongeunChoi@users.noreply.github.com>
2023-07-10 00:05:23 +0900

    exception: 이메일토큰에러 설정
    - 기존의 TokenException 에러 대신 EmailInvalidTokenException으로 변경
    - 해당 에러에 대해 html 반환이 필요하여 따로 생성하여 적용
    
    related to: #151

--- src/main/java/com/aliens/friendship/domain/emailAuthentication/exception/EmailAuthenticationExceptionCode.java
+++ src/main/java/com/aliens/friendship/domain/emailAuthentication/exception/EmailAuthenticationExceptionCode.java
@@ -12,7 +12,8 @@ import static org.springframework.http.HttpStatus.*;
 public enum EmailAuthenticationExceptionCode implements ExceptionCode {
 
     EMAIL_ALREADY_REGISTERED(BAD_REQUEST, "EAT-C-001", "이미 회원가입된 이메일입니다."),
-    EMAIL_VERIFICATION_TIME_OUT(UNAUTHORIZED, "EAT-C-002", "이메일 인증 시간이 초과되었습니다.");
+    EMAIL_VERIFICATION_TIME_OUT(UNAUTHORIZED, "EAT-C-002", "이메일 인증 시간이 초과되었습니다."),
+    EMAIL_INVALID_TOKEN(UNAUTHORIZED, "EAT-C-003", "유효하지 않은 이메일 인증 토큰입니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
