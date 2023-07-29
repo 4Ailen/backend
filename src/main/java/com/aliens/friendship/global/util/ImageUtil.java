a2d535bd84bde44dd64a99d673f41b14b3d80bc3
김명준 <86913355+mjj111@users.noreply.github.com>
2023-07-30 02:01:26 +0900

    refactor : 멀티모듈 적용으로인한 위치변경
    #162

--- src/main/java/com/aliens/friendship/global/util/ImageUtil.java
+++
@@ -1,8 +0,0 @@
-package com.aliens.friendship.global.util;
-
-public class ImageUtil {
-
-    public static String getImageExtension(String imageFileName) {
-        return imageFileName.substring(imageFileName.lastIndexOf(".") + 1);
-    }
-}
