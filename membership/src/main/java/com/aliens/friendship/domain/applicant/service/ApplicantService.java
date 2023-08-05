2fa961ec2784a06093b7741ac7b6ca0b75fe9b3f
김명준 <86913355+mjj111@users.noreply.github.com>
2023-08-05 16:02:27 +0900

    refactor : 매칭 예정 날짜반환 메서드 변경
    #181

--- membership/src/main/java/com/aliens/friendship/domain/applicant/service/ApplicantService.java
+++ membership/src/main/java/com/aliens/friendship/domain/applicant/service/ApplicantService.java
@@ -32,30 +32,54 @@ public class ApplicantService {
         return applicantEntity;
     }
 
+    /**
+     v1 기준 매칭날짜(금요일) 반환
+     */
     public Instant getDateWillMatched(ApplicantEntity applicantEntity) {
         Instant applicationDateInstant = applicantEntity.getCreatedAt();
         ZoneId zoneId = ZoneId.systemDefault();
         LocalDate applicationDate = applicationDateInstant.atZone(zoneId).toLocalDate();
         DayOfWeek dayOfWeek = applicationDate.getDayOfWeek();
 
-        // applicationDate가 화요일, 수요일, 목요일에 속하는 경우, 그 주의 금요일 00시를 반환
-        if (dayOfWeek == DayOfWeek.TUESDAY || dayOfWeek == DayOfWeek.WEDNESDAY || dayOfWeek == DayOfWeek.THURSDAY) {
-            ZonedDateTime nextFriday = applicationDate.with(TemporalAdjusters.next(DayOfWeek.FRIDAY)).atTime(LocalTime.MIN).atZone(ZoneId.systemDefault());
-            return nextFriday.toInstant();
-
+        // applicationDate가 금요일 이전인 경우, 그 주의 금요일 00시를 반환
+        if (dayOfWeek.compareTo(DayOfWeek.FRIDAY) < 0) {
+            return getThisFriday(applicationDate);
         }
-
-        //월요일인 경우 (내일) 화요일 00시 반환
-        else if (dayOfWeek == DayOfWeek.MONDAY) {
-            ZonedDateTime tomorrow = applicationDate.with(TemporalAdjusters.next(DayOfWeek.TUESDAY)).atTime(LocalTime.MIN).atZone(ZoneId.systemDefault());
-            return tomorrow.toInstant();
-        }
-
-        else { // 그 외 (금요일, 토요일, 일요일), 다음 주의 화요일 00시를 반환
-            ZonedDateTime nextTuesday = applicationDate.with(TemporalAdjusters.next(DayOfWeek.TUESDAY)).atTime(LocalTime.MIN).atZone(ZoneId.systemDefault());
-            return nextTuesday.toInstant();
+        // 금요일 이후인 경우, 다음 주의 금요일 00시를 반환
+        else {
+            LocalDate nextFriday = applicationDate.with(TemporalAdjusters.next(DayOfWeek.FRIDAY));
+            return getNextFriday(nextFriday);
         }
     }
+
+
+    /**
+     v2 기준 매칭날짜(화요일, 금요일) 반환
+     */
+//    public Instant getDateWillMatched(ApplicantEntity applicantEntity) {
+//        Instant applicationDateInstant = applicantEntity.getCreatedAt();
+//        ZoneId zoneId = ZoneId.systemDefault();
+//        LocalDate applicationDate = applicationDateInstant.atZone(zoneId).toLocalDate();
+//        DayOfWeek dayOfWeek = applicationDate.getDayOfWeek();
+//
+//        // applicationDate가 화요일, 수요일, 목요일에 속하는 경우, 그 주의 금요일 00시를 반환
+//        if (dayOfWeek == DayOfWeek.TUESDAY || dayOfWeek == DayOfWeek.WEDNESDAY || dayOfWeek == DayOfWeek.THURSDAY) {
+//            ZonedDateTime nextFriday = applicationDate.with(TemporalAdjusters.next(DayOfWeek.FRIDAY)).atTime(LocalTime.MIN).atZone(ZoneId.systemDefault());
+//            return nextFriday.toInstant();
+//
+//        }
+//
+//        //월요일인 경우 (내일) 화요일 00시 반환
+//        else if (dayOfWeek == DayOfWeek.MONDAY) {
+//            ZonedDateTime tomorrow = applicationDate.with(TemporalAdjusters.next(DayOfWeek.TUESDAY)).atTime(LocalTime.MIN).atZone(ZoneId.systemDefault());
+//            return tomorrow.toInstant();
+//        }
+//
+//        else { // 그 외 (금요일, 토요일, 일요일), 다음 주의 화요일 00시를 반환
+//            ZonedDateTime nextTuesday = applicationDate.with(TemporalAdjusters.next(DayOfWeek.TUESDAY)).atTime(LocalTime.MIN).atZone(ZoneId.systemDefault());
+//            return nextTuesday.toInstant();
+//        }
+//    }
 
     public List<ApplicantEntity> findAllParticipants() {
         List<ApplicantEntity> applicantEntities = applicantRepository.findAllByIsMatched(ApplicantEntity.Status.NOT_MATCHED);
@@ -76,5 +100,19 @@ public class ApplicantService {
     public void changePreferLanguages(ApplicantEntity applicantEntity, ApplicantRequestDto applicantRequestDto) {
         applicantEntity.updatePreferLanguages(ApplicantEntity.Language.valueOf(applicantRequestDto.getFirstPreferLanguage()), ApplicantEntity.Language.valueOf(applicantRequestDto.getSecondPreferLanguage()));
         applicantRepository.save(applicantEntity);
+    }
+
+    public static Instant getThisFriday(LocalDate date) {
+        return date.with(TemporalAdjusters.nextOrSame(DayOfWeek.FRIDAY)) // 이번 주 금요일 반환
+                .atTime(LocalTime.MIN)
+                .atZone(ZoneId.systemDefault())
+                .toInstant();
+    }
+
+    public static Instant getNextFriday(LocalDate date) {
+        return date.with(TemporalAdjusters.nextOrSame(DayOfWeek.FRIDAY)) // 다음 주 금요일 반환
+                .atTime(LocalTime.MIN)
+                .atZone(ZoneId.systemDefault())
+                .toInstant();
     }
 }
