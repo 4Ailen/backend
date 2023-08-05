7c7e6882e68ed4fbca9ea69b18f60b4230b13d15
김명준 <86913355+mjj111@users.noreply.github.com>
2023-08-05 16:08:38 +0900

    refactor : 회원 상태변경에 따른 메서드 추가
    #181

--- db/src/main/java/com/aliens/db/member/repository/MemberRepository.java
+++ db/src/main/java/com/aliens/db/member/repository/MemberRepository.java
@@ -3,6 +3,7 @@ package com.aliens.db.member.repository;
 import com.aliens.db.member.entity.MemberEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
@@ -10,4 +11,6 @@ public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
     Optional<MemberEntity> findById(Long memberId);
     boolean existsByEmail(String email);
     Optional<MemberEntity> findByEmailAndName(String email, String name);
+
+    List<MemberEntity> findAllByStatus(MemberEntity.Status applied);
 }
