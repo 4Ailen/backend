6d211649ca220be539a21fb3b000139c5e451e53
edgar6bf <edgar6bf@gmail.com>
2023-08-13 20:08:11 +0900

    refactor: #196 BaseEntity 추상클래스로 변경

--- db/src/main/java/com/aliens/db/BaseEntity.java
+++ db/src/main/java/com/aliens/db/BaseEntity.java
@@ -11,7 +11,7 @@ import java.time.Instant;
 
 @MappedSuperclass @SuperBuilder @Getter
 @NoArgsConstructor @AllArgsConstructor
-public class BaseEntity {
+public abstract class BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
