4af782ab2d154abca3ffee6081ec1e55fabceb7e
JeongeunChoi <77786996+JeongeunChoi@users.noreply.github.com>
2023-09-03 21:54:31 +0900

    feat: 멤버id에 따른 fcm 토큰 가져오기
    related to: #203

--- db/src/main/java/com/aliens/db/auth/repository/FcmTokenRepository.java
+++ db/src/main/java/com/aliens/db/auth/repository/FcmTokenRepository.java
@@ -5,10 +5,12 @@ import com.aliens.db.auth.entity.RefreshTokenEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface FcmTokenRepository extends MongoRepository<FcmTokenEntity, String> {
     void deleteAllByValue(String value);
     FcmTokenEntity findByValue(String fcmToken);
+    List<FcmTokenEntity> findAllByMemberId(Long memberId);
 }
