a65388cda6cce918c972741610266bf8166f7c57
JeongeunChoi <77786996+JeongeunChoi@users.noreply.github.com>
2023-10-11 23:35:07 +0900

    exception: 게시글 5분 내에 재생성 불가능하도록 예외 추가
    related to: #235

--- membership/src/main/java/com/aliens/friendship/domain/article/exception/ArticleExceptionCode.java
+++ membership/src/main/java/com/aliens/friendship/domain/article/exception/ArticleExceptionCode.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @Getter
@@ -13,7 +14,8 @@ public enum ArticleExceptionCode
         implements ExceptionCode {
 
     ARTICLE_NOT_FOUND(NOT_FOUND, "BA-C-001", "존재하지 않는 게시글입니다."),
-    ARTICLE_COMMENT_NOT_FOUND(NOT_FOUND, "BA-C-001", "존재하지 않는 게시글 댓글입니다.");
+    ARTICLE_COMMENT_NOT_FOUND(NOT_FOUND, "BA-C-001", "존재하지 않는 게시글 댓글입니다."),
+    ARTICLE_CREATION_NOT_ALLOWED(BAD_REQUEST, "BA-C-003", "게시글 생성 후 5분 후에 재생성이 가능합니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
